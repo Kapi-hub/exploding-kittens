d040cb0ffd759770c9426391c8c2eef76efb1ccc
Culda, A.D. (Alexandru, Student B-BIT) <a.culda@student.utwente.nl>
2024-01-23 20:23:06 +0100

    Testing to see if commit and push works. Previously, GIT would return a 500 internal error. V6

--- src/local/controller/GameController.java
+++ src/local/controller/GameController.java
@@ -159,7 +159,7 @@ public class GameController {
     public static void raiseWarningFavorNoThirdArg() {
         TUI.raiseWarningFavorNoThirdArg();
         askAndSendInputForProcessing();
-    } // testing this branch after merging
+    } // testing this branch after merging V2
 
     public static void raiseWarningFavorPlayerNotFound() {
         TUI.raiseWarningFavorPlayerNotFound();
